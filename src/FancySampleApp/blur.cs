6b75428f88adb57646d03dc7557367ca6ed1936b
jpmikkers <jean.paul.mikkers@gmail.com>
2024-11-04 12:49:49 +0100

    Target win11 sdk 10.0.26100.0

---
+++ src/FancySampleApp/blur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinUIEx;
+
+namespace FancySampleApp;
+public partial class BlurredBackdrop : CompositionBrushBackdrop
+{
+    protected override Windows.UI.Composition.CompositionBrush CreateBrush(Windows.UI.Composition.Compositor compositor)
+        => compositor.CreateHostBackdropBrush();
+}
+
+public class MyClass
+{
+    public MyClass()
+    {
+        var x = new TransparentTintBackdrop();
+    }
+}
